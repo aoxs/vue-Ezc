59a37bc5291f33d47f4faf50616541ada997156d
Liu Qinqiu <aoxs_98@163.com>
2020-10-28 18:15:02 +0800

    引入地图

---
+++ src/views/proMap/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="dashboard-editor-container">
+
+    <iframe id="aoxs_mian"
+            src="http://m.wuceproject.com/wckj/xg/theme/index.html"
+            frameborder="0"
+            width="100%"
+            scrolling="no"
+            :height="ifHeight"></iframe>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      ifHeight: 0
+    }
+  },
+  methods: {
+    changeTableMaxHeight() {
+      let height = document.body.offsetHeight // 网页可视区域高度
+      // if (this.filterActive) {
+      //   this.tableMaxHeight = height - 320
+      // } else {
+      this.tableMaxHeight = height - 85
+      // }
+      console.log(this.tableMaxHeight)
+      this.ifHeight = this.tableMaxHeight
+    }
+  },
+  created() {
+
+    this.changeTableMaxHeight()
+
+  },
+  mounted() {
+    window.onresize = () => {
+      this.changeTableMaxHeight()
+    }
+  },
+  destroyed() {
+    window.onresize = null
+  },
+}
+</script>
+
+<style>
+</style>
