7643335b1947876adf8ea7b59a78244ee2b7cdb0
Liu Qinqiu <aoxs_98@163.com>
2020-11-13 14:24:18 +0800

    签约签约

--- src/views/statistical/signing.vue
+++ src/views/statistical/signing.vue
@@ -1,37 +1,69 @@
 <template>
   <div class="dashboard-editor-container">
-    <div style="background-color: #fff">
-      <div style="padding:20px 10px;display:flex;justify-content: space-around">
+    <i class="el-icon-s-platform" id="full_screen" />
+    <div style="background-color: #fff;border: 1px solid #DADADA;"
+         id="signing">
+      <div style="padding:20px 10px;display:flex;justify-content: space-around;background-color:#F5F5F5;border-bottom:1px solid #DADADA">
         <div>英联E征拆签约实时统计</div>
         <div>{{ nowDate }}</div>
         <div>已签约：xx/xx</div>
         <div>占比：50%</div>
       </div>
-    <!-- <el-button type="primary" size="default" @click="aaa">确定</el-button> -->
-    
-    
+      <!-- <el-button type="primary" size="default" @click="aaa">确定</el-button> -->
+      <el-table :data="tableData"
+                border
+                stripe
+                style="margin:10px auto;width:98%">
+        <el-table-column prop="sigTeam"
+                         label="小组"
+                         width="">
+        </el-table-column>
+        <el-table-column prop="sigPeople"
+                         label="权利人"
+                         width="">
+        </el-table-column>
+        <el-table-column prop="sigDate"
+                         label="签约时间"
+                         width="">
+        </el-table-column>
+        <el-table-column prop="sigPhoto"
+                         label="签约照片"
+                         width="">
+          <template slot-scope="scope">
+            　　　　<img :src="scope.row.sigPhoto"
+                 width="100%" />
+            　　</template>
+        </el-table-column>
+        <el-table-column prop="sigRanking"
+                         label="排名"
+                         width="">
+        </el-table-column>
+
+      </el-table>
+
     </div>
   </div>
 </template>
 <script>
+import screenfull from "screenfull"; 
 export default {
+  
   data() {
     return {
+      isFullscreen: false,
       Server_Date: '',
       nowDate: "", // 当前日期
+      tableData: [
+        { sigTeam: '一组', sigPeople: "张三", sigDate: "这里是上传时间", sigPhoto: "https://vkceyugu.cdn.bspapp.com/VKCEYUGU-aoxs/4eec6c80-0aa9-11eb-b244-a9f5e5565f30.png", sigRanking: 23 },
+        { sigTeam: '二组', sigPeople: "李四", sigDate: "这里是上传时间", sigPhoto: "https://vkceyugu.cdn.bspapp.com/VKCEYUGU-aoxs/4eec6c80-0aa9-11eb-b244-a9f5e5565f30.png", sigRanking: 22 },
+      ],
     };
   },
   methods: {
-    aaa(){
-      var dd = new Date();
-      console.log(dd)
-    },
     currentTime() {
       setInterval(this.formatDate, 100);
     },
     formatDate() {
-
-     
       let date = new Date()
       let year = date.getFullYear(); // 年
       let month = date.getMonth() + 1; // 月
@@ -48,13 +80,20 @@ export default {
     }
   },
   created() {
-    
+
   },
   mounted() {
     this.currentTime();
+    const element = document.getElementById('signing');
+    document.getElementById('full_screen').addEventListener('click', () => {
+     
+      if (screenfull.enabled) {
+        screenfull.request(element); // 元素全屏
+      }
+    });
 
   },
- 
+
   beforeDestroy() {
     if (this.formatDate) {
       clearInterval(this.formatDate); //清除时间定时器
@@ -63,4 +102,5 @@ export default {
 }
  </script>
 <style lang="scss" scoped>
+
 </style>
